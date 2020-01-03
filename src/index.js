f02ccd177fd629fe3038fbfcb3c210d6902199a6
kowoohyuk <kowoohyuk91@gmail.com>
2020-01-03 18:16:05 +0900

    temp

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {this.props.value}
       </button>
     );
   }
@@ -14,12 +14,11 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
     const status = 'Next player: X';
-
     return (
       <div>
         <div className="status">{status}</div>
